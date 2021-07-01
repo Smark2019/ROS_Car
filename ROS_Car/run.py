418bac873803c30861a6b276da4eb17077f38fb5
Smark2019 <salih8461921@outlook.com>
2021-07-01 15:45:37 +0300

    The scripts ready done to be used.

--- ROS_Car/run.py
+++ ROS_Car/run.py
@@ -1,4 +1,4 @@
-import Rpi.GPIO
+import Rpi.GPIO as GPIO
 from time import sleep
 
 class MotorGroup():
@@ -8,6 +8,7 @@ class MotorGroup():
         self.in1 = in1
         self.in2 = in2
         # pin Setup
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.en, GPIO.OUT)
         GPIO.setup(self.in1, GPIO.OUT)
         GPIO.setup(self.in2, GPIO.OUT)
@@ -15,17 +16,31 @@ class MotorGroup():
         # pwm settings
         self.pwm = GPIO.PWM(self.en,100)
         self.pwm.start(0)
-    def forward():
-        pass
 
-    def backward():
-        pass
+    def forward(self, pwm = 50):
+        self.in1 = HIGH
+        self.in2 = LOW
+        self.pwm.changeDutyCycle(pwm)
 
-    def stop():
-        pass
+    def backward(self,pwm = 50):
+        self.in1 = LOW
+        self.in2 = HIGH
+        self.pwm.changeDutyCycle(pwm)
+
+    def stop(self):
+        self.in1 = LOW
+        self.in2 = LOW
+        
+                
 
 
 
-    if __name__ == "__main__":
-        pass
+if __name__ == "__main__":
+
+    motorGroup_left = MotorGroup(22,4,27)
+    motorGroup_left.forward()
+    sleep(5)
+    motorGroup_left.stop()
+   # motorGroup_right = MotorGroup(22,4,27)
+        
 
