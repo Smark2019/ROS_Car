982e102163f6c5371f9ab2b08f89f75398ade61a
Smark2019 <salih8461921@outlook.com>
2021-07-01 15:01:34 +0300

    MotorGroups class genereted and waits to be done.

---
+++ ROS_Car/run.py
@@ -0,0 +1,21 @@
+
+
+class MotorGroup():
+    def __init__(self) -> None:
+        pass
+
+    def forward():
+        pass
+
+    def backward():
+        pass
+
+    def stop():
+        pass
+
+
+
+    if __name__ == "__main__":
+        pass
+
+    
