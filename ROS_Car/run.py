b78b0d41c4f080b008c15a3754a3f80b968d24ea
Smark2019 <salih8461921@outlook.com>
2021-07-01 15:25:59 +0300

    class attrbs and pin & pwm settings done.

--- ROS_Car/run.py
+++ ROS_Car/run.py
@@ -1,9 +1,20 @@
-
+import Rpi.GPIO
+from time import sleep
 
 class MotorGroup():
-    def __init__(self) -> None:
-        pass
+    def __init__(self,en,in1,in2):
+        # class attributes
+        self.en = en
+        self.in1 = in1
+        self.in2 = in2
+        # pin Setup
+        GPIO.setup(self.en, GPIO.OUT)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
 
+        # pwm settings
+        self.pwm = GPIO.PWM(self.en,100)
+        self.pwm.start(0)
     def forward():
         pass
 
@@ -18,4 +29,3 @@ class MotorGroup():
     if __name__ == "__main__":
         pass
 
-    
